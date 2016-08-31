2bc9f48089c494e28e2731dc7341838b72e89490
Abel <abel.tamayo@gmail.com>
2016-08-31 11:56:48 +0200

    Refactored reducer.

--- src/reducers/shoppingCartReducer.js
+++ src/reducers/shoppingCartReducer.js
@@ -3,19 +3,40 @@ import * as consts from '../constants'
 import { subtotalSelector, vatSelector, shippingCostSelector, shoppingCartTotalSelector }
   from '../selectors/ShoppingCartSelector'
 
+const addToCart = (state, id) => {
+  let clonedItems = JSON.parse(JSON.stringify(state.items))
+  const index = clonedItems.findIndex((elem) => elem.id === id)
+  if (index >= 0) {
+    let product = clonedItems[index]
+    product.quantity++
+    product.promo = new Array(Math.floor(product.quantity / 3)).fill(consts.PROMOS.EXTRA_ITEM)
+    product.gift = Math.floor(product.quantity / 3)
+  } else {
+    clonedItems.push({ id: id, quantity: 1, gift: 0, promo: [ ] })
+  }
+  return clonedItems
+}
+
+const removeFromCart = (state, id) => {
+  let clonedItems = JSON.parse(JSON.stringify(state.items))
+  const index = clonedItems.findIndex((elem) => elem.id === id)
+  if (index >= 0) {
+    let product = clonedItems[index]
+    if (product.quantity > 1) {
+      product.quantity--
+      product.promo = new Array(Math.floor(product.quantity / 3)).fill(consts.PROMOS.EXTRA_ITEM)
+      product.gift = Math.floor(product.quantity / 3)
+    } else {
+      clonedItems.splice(index, 1)
+    }
+  }
+  return clonedItems
+}
+
 export default function shoppingCartReducer (state = { items: [ ] }, action) {
   switch (action.type) {
     case types.ADD_ITEM_TO_SHOPPING_CART: {
-      let clonedItems = JSON.parse(JSON.stringify(state.items))
-      const index = clonedItems.findIndex((elem) => elem.id === action.id)
-      if (index >= 0) {
-        let product = clonedItems[index]
-        product.quantity++
-        product.promo = new Array(Math.floor(product.quantity / 3)).fill(consts.PROMOS.EXTRA_ITEM)
-        product.gift = Math.floor(product.quantity / 3)
-      } else {
-        clonedItems.push({ id: action.id, quantity: 1, gift: 0, promo: [ ] })
-      }
+      let clonedItems = addToCart(state, action.id)
       const prevState = action.getState()
       prevState.shoppingCart.items = clonedItems
       return {
@@ -27,18 +48,7 @@ export default function shoppingCartReducer (state = { items: [ ] }, action) {
       }
     }
     case types.REMOVE_ITEM_FROM_SHOPPING_CART: {
-      let clonedItems = JSON.parse(JSON.stringify(state.items))
-      const index = clonedItems.findIndex((elem) => elem.id === action.id)
-      if (index >= 0) {
-        let product = clonedItems[index]
-        if (product.quantity > 1) {
-          product.quantity--
-          product.promo = new Array(Math.floor(product.quantity / 3)).fill(consts.PROMOS.EXTRA_ITEM)
-          product.gift = Math.floor(product.quantity / 3)
-        } else {
-          clonedItems.splice(index, 1)
-        }
-      }
+      const clonedItems = removeFromCart(state, action.id)
       const prevState = action.getState()
       prevState.shoppingCart.items = clonedItems
       return {
